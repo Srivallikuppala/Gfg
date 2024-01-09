20cf52a72740399d665598407ae67943c506fae2
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2024-01-09 21:40:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def search(self, pat, txt):
+        # code here
+        l=[]
+        a = len(txt)
+        b = len(pat)
+        for i in range((a-b)+1):
+            if txt[i]==pat[0] and txt[i:i+b] == pat:
+                l.append(i+1)
+        return l
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        patt = input().strip()
+        ob = Solution()
+        ans = ob.search(patt, s)
+        if len(ans)==0:
+            print(-1,end="")
+        for value in ans:
+            print(value,end = ' ')
+        print()
+# } Driver Code Ends
