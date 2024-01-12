2a049c03208dcfa5276fcff867082cfd8f988f2e
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2024-01-12 23:27:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sieve of Eratosthenes/sieve-of-eratosthenes.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def sieveOfEratosthenes(self, N):
+    	#code here 
+    	y = [1 for i in range(N+1)]
+    	y[0]=y[1]=0
+    	for i in range(2,N+1):
+    	    if i!=0:
+    	        for j in range(i*i,N+1,i):
+    	            y[j]=0
+    	r = []
+    	for i in range(len(y)):
+    	    if y[i]!=0:r.append(i)
+    	return r
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.sieveOfEratosthenes(N)
+        for i in ans:
+            print(i, end=" ")
+        print()
+# } Driver Code Ends
