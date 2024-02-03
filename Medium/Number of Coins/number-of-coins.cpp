f63b3c22e5cecf05122ae573222fda600d887f02
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2024-02-03 16:56:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Coins/number-of-coins.cpp
+++ Medium/Number of Coins/number-of-coins.cpp
@@ -6,28 +6,27 @@ using namespace std;
 class Solution{
 
 	public:
-	long long d[1000001];
-	long long fun(vector<int>c,int cnt ,int res){
-	    if(d[res]!=-1) return d[res];
-	    if(res==0) return 0;
-	    long long ans = INT_MAX;
-	    for(int i = 0 ; i < cnt ; i++){
-	        //cout<<ans<<" ";
-	        if(res>=c[i])ans = min(ans,fun(c,cnt,res-c[i])+1);
-	        //cout<<ans<<" ";
+	long long dp[99][10002];
+	long long fun(vector<int>c,int cnt ,int res , int idx ){
+	    if(idx >= cnt){
+	        if(res == 0) return 0;
+	        return INT_MAX;
 	    }
-	    d[res]=ans;
-	   // ans = min(ans,fun(c,cnt,res-c[idx],idx))+1;
-	   // ans = min(ans,fun(c,cnt,res,idx+1))+1;
-	   // cout<<ans<<" "<<idx<<" ";
+	    if(dp[idx][res]!=-1){
+	        return dp[idx][res];
+	    }
+	    long long ans = INT_MAX;
+	    ans = min(ans,fun(c,cnt,res,idx+1));
+	    if(res>=c[idx]) ans = min(ans,1+fun(c,cnt,res-c[idx],idx));
+	    dp[idx][res] = ans;
 	    return ans;
 	}
 	int minCoins(vector<int> &coins, int cnt, int res) 
 	{ 
-	    // Your code goes here
-	    memset(d,-1,sizeof(d));
-	    if(*min_element(coins.begin(),coins.end())>res) return -1;
-	    long long x = fun(coins,cnt,res);
+	   //vector<vector<long long>>d(cnt,vector<long long>(res,-1));
+	   memset(dp,-1,sizeof(dp)) ;
+	    long long x = fun(coins,cnt,res,0);
+	   
 	    if(x==INT_MAX) return -1;
 	    return x;
 	} 
