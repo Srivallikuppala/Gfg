b53c7e1f70cdb603e40b6809ac7e2f5897d4494c
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2024-02-19 17:40:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Josephus problem/josephus-problem.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int josephus(int n, int k)
+    {
+       //Your code 
+       int p = 0 ;
+       for(int i = 1 ; i < n+1 ; i++){
+           p=(p+k)%i;
+       }
+       return p+1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
